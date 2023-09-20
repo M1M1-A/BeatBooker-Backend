75f5530259b0efdcc1e3a4db53e8f8e94b49fe8f
sam <sam.ryan101@gmail.com>
2023-09-20 15:30:09 +0100

    api setup followed

--- backend/beatbooker/views.py
+++ backend/beatbooker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import BeatbookerSerializer
+from .models import User
+
+class UserView(viewsets.ModelViewSet):
+    serializer_class = BeatbookerSerializer
+    queryset = User.objects.all()
