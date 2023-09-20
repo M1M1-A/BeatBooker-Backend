2abf2b332a2f74491238ed2d4191a2a8bb7b6ca3
sam <sam.ryan101@gmail.com>
2023-09-20 16:02:16 +0100

    started writing tests to backend

--- backend/beatbooker/models.py
+++ backend/beatbooker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=120)
-    email = models.CharField(max_length=120)
+    email = models.EmailField()
     firstname = models.CharField(max_length=120)
     lastname = models.CharField(max_length=120)
     
--- backend/beatbooker/tests.py
+++ backend/beatbooker/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(name="test", email="testuser@.com", firstname="test", lastname="user")
+        User.objects.create(name="test2", email="testuser2@.com", firstname="test2", lastname="user2")
+
+    def test_Users_have_names(self):
+        """Users that have names should return True"""
+        test1 = User.objects.get(name="test")
+        test2 = User.objects.get(name="test2")
+        self.assertEqual(test1.name, 'test')
+        self.assertEqual(test2.name, 'test2')
+
+    def test_Users_have_emails_firstnames_lastnames(self):
+        test1 = User.objects.get(name="test")
+        test2 = User.objects.get(name="test2")
+        self.assertEqual(test1.email, 'testuser@.com')
+        self.assertEqual(test2.email, 'testuser2@.com')
+        self.assertEqual(test1.firstname, 'test')
+        self.assertEqual(test2.firstname, 'test2')
+        self.assertEqual(test1.lastname, 'user')
+        self.assertEqual(test2.lastname, 'user2')
+
+    def test_creating_user_incorrect_email(self):
+        with self.assertRaises(ValueError):
+            User.objects.create(name="test3", email="testuser3.com", firstname="test3", lastname="user3")
+
